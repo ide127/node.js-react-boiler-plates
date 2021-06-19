7087d3e968abd4c28013f35e4500390bd665d7f0
Gonglee Kim <gongleekim@Gongleeui-MacBookPro.local>
2021-06-19 20:28:39 +0900

    config 폴더를 만들어 dev 파일과 prod 파일을 생성했습니다. 그리고 비밀정보 보호를 위해 mongoURI를 불러오는 방법을 dev와 prod 환경에 따라 달리 하였습니다.

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const { User } = require("./models/User")
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({exteded: true}));
+//application/json
+app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gonglee:gonglee@cluster0.ipkyk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect( config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello world! This is my place!'))
+
+app.post('/register', (req, res) => {
+    //회원가입시 필요한 정보들을 client에서 가져오고 MongoDB에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ 
+            success: false, err
+        })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
